91b93dfc0099348b1399b67e803bf36d073debc2
SD <sashaluckyman@gmail.com>
2021-12-29 17:53:13 -0800

    add mock data and render basic info for the widgets and people

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,23 @@
 import React from 'react';
+import widgets from './mock-data/widgets';
+import people from './mock-data/people';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-      </header>
-    </div>
+    <>
+      <h2>Widgets:</h2>
+      {widgets.map(widget => {
+        return (
+          <h3>{widget.title}</h3>
+        )
+      })}
+      <h2>People:</h2>
+      {people.map(person => {
+        return (
+          <h3>{person.firstName} {person.lastName}</h3>
+        )
+      })}
+    </>
   );
 }
 
