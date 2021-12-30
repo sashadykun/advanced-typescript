2c0f2750e8eaaa731552c55657fd438ddfc830ac
SD <sashaluckyman@gmail.com>
2021-12-30 15:20:49 -0800

    add generic sorting function and render sorting input

--- src/App.tsx
+++ src/App.tsx
@@ -3,27 +3,56 @@ import widgets from './mock-data/widgets';
 import people from './mock-data/people';
 import genericSearch from './utils/genericSearch';
 import { SearchInput } from './components/SearchInput';
+import genericSort from './utils/genericSort';
+import IProperty from './interfaces/IProperty';
+import IWidget from './interfaces/IWidget';
+import IPerson from './interfaces/IPerson';
+import { Sorters } from './components/Sorters';
 
 function App() {
   // const query = '';
   const [query, setQuery] = useState<string>('');
+  const [widgetSortProperty, setWidgetSortProperty] = useState<IProperty<IWidget>>({ property: 'title' });
+  const [peopleSortProperty, setPeopleSortProperty] = useState<IProperty<IPerson>>({ property: 'lastName' });
   return (
     <>
       <SearchInput setSearchQuery={(query) => {
         console.log('I am firing!')
-        setQuery(query)}} />
+        setQuery(query)
+      }} />
       <h2>Widgets:</h2>
-      {widgets.filter((widget => genericSearch(widget, ['title', 'description'], query, true))).map(widget => {
-        return (
-          <h3>{widget.title}</h3>
+      <Sorters
+        setProperty={(property) => 
+          setWidgetSortProperty({property})
+        }
+        object={widgets[0]}
+      />
+      {widgets
+        .filter((widget => genericSearch(widget, ['title', 'description'], query, false))
         )
-      })}
+        .sort((a, b) =>
+          genericSort(a, b, widgetSortProperty.property)
+        )
+        .map(widget => {
+          return (
+            <h3>{widget.title}</h3>
+          )
+        })}
       <h2>People:</h2>
-      {people.filter((person => genericSearch(person, ['firstName', 'lastName', 'eyeColor'], query, true))).map(person => {
-        return (
-          <h3>{person.firstName} {person.lastName}</h3>
+      <Sorters setProperty={(property) => 
+        setPeopleSortProperty({property})
+      } object={people[0]} />
+      {people
+        .filter((person => genericSearch(person, ['firstName', 'lastName', 'eyeColor'], query, false))
         )
-      })}
+        .sort((a, b) =>
+          genericSort(a, b, peopleSortProperty.property)
+        )
+        .map(person => {
+          return (
+            <h3>{person.firstName} {person.lastName}</h3>
+          )
+        })}
     </>
   );
 }
